62a8b85b3f8f06cb6509bd5d518e94ed57318461
Elena-Shannon <elenashannon@byui.edu>
2020-09-26 11:19:33 -0600

    Design Principles

---
+++ lesson2/js/webDesign.js
@@ -0,0 +1,7 @@
+var date = new Date().getFullYear();
+
+document.getElementById("year").innerHTML = date;
+
+var modiDate = document.lastModified;
+
+document.getElementById("modiDate").innerHTML = modiDate;
